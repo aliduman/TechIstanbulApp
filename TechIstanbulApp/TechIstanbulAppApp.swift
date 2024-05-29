8f47f2fc06162735cc38ae60d150af64d5284352
Ali Duman <alidumanpr@hotmail.com>
2024-05-29 20:05:02 +0300

    Initial Commit

---
+++ TechIstanbulApp/TechIstanbulAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechIstanbulAppApp.swift
+//  TechIstanbulApp
+//
+//  Created by Ali Duman on 29.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TechIstanbulAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
